120a0dc671d5230a6451c694ef6eb3ffcf8b89df
chusj <chusj.mail@qq.com>
2024-07-09 19:57:37 +0800

    新增用户仓储Demo 仓储接口、仓储类  负责跟DB交互

---
+++ Relay.Repository/ISysUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Relay.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relay.Repository
+{
+    internal interface ISysUserRepository
+    {
+        Task<List<SysUser>> Query();
+    }
+}
---
+++ Relay.Repository/SysUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Relay.Model;
+
+namespace Relay.Repository
+{
+    public class SysUserRepository : ISysUserRepository
+    {
+        public async Task<List<SysUser>> Query()
+        {
+            await Task.CompletedTask;
+
+            var data = "[{\"FullName\":\"xiaochu\"}]";
+
+            return JsonConvert.DeserializeObject<List<SysUser>>(data) ?? new List<SysUser>();
+        }
+    }
+}
