b86b208d1772c1aa0fb756825142c62f24a95807
chusj <chusj.mail@qq.com>
2024-07-09 20:04:31 +0800

    Service服务层可用于中转，通过视图模型，避免暴漏仓储层的数据库相关信息

--- Relay.IService/ISysUserService.cs
+++ Relay.IService/ISysUserService.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Relay.Model;
 
 namespace Relay.IService
 {
     internal interface ISysUserService
     {
+        Task<List<SysUserVo>> Query();
     }
 }
---
+++ Relay.Model/SysUserVo.cs
@@ -0,0 +1,14 @@
+﻿namespace Relay.Model
+{
+    /// <summary>
+    /// 系统用户视图模型
+    /// </summary>
+    public class SysUserVo
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string FullName { get; set; }
+
+    }
+}
