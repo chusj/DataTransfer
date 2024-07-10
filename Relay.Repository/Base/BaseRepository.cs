07c9d6b41d790f75f0833a9178f587787d30ca4c
chusj <chusj.mail@qq.com>
2024-07-10 14:36:28 +0800

    第7，新增仓储接口和仓储基类

---
+++ Relay.Repository/Base/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Relay.Repository
+{
+    /// <summary>
+    /// 仓储基类
+    /// </summary>
+    public class BaseRepository : IBaseRepository<TEntity> where TEntity : class, new()
+    {
+        public async Task<List<TEntity>> Query()
+        {
+            await Task.CompletedTask;
+            var data = "[{\"Id\": 18,\"Name\":\"namenamename\"}]";
+            return JsonConvert.DeserializeObject<List<TEntity>>(data) ?? new List<TEntity>();
+        }
+    }
+}
---
+++ Relay.Repository/Base/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Relay.Repository
+{
+    /// <summary>
+    /// 仓储基类接口
+    /// </summary>
+    internal interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>> Query();
+    }
+}
