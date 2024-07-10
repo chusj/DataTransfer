4a813cada3ea3d9237286688545954ba5ca099a8
chusj <chusj.mail@qq.com>
2024-07-10 17:14:40 +0800

    在服务基类中，注入仓储基类接口

--- Relay.Service/BaseService.cs
+++ Relay.Service/BaseService.cs
@@ -15,16 +15,18 @@ namespace Relay.Service
     public class BaseService<TEntity, TVo> : IBaseService<TEntity, TVo> where TEntity : class, new()
     {
         private readonly IMapper _mapper;
+        private readonly IBaseRepository<TEntity> _baseRepository;
 
-        public BaseService(IMapper mapper)
+        public BaseService(IMapper mapper,IBaseRepository<TEntity> baseRepository)
         {
             _mapper = mapper;
+            _baseRepository = baseRepository;
         }
 
         public async Task<List<TVo>> Query()
         {
-            var baseRepo = new BaseRepository<TEntity>();
-            var entities = await baseRepo.Query();
+            //var baseRepo = new BaseRepository<TEntity>();
+            var entities = await _baseRepository.Query();
             var llout = _mapper.Map<List<TVo>>(entities);
             return llout;
         }
