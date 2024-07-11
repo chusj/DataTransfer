b3edc36b9526043a88c93bf6d0fc402650e76031
chusj <chusj.mail@qq.com>
2024-07-11 14:09:34 +0800

    10.2 配置类文件和限制性接口以及Redis配置文件

---
+++ Relay.Common/Option/IConfigurableOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Relay.Common
+{
+    /// <summary>
+    /// 配置类的统一接口 (限制性接口)
+    /// </summary>
+    public interface IConfigurableOptions
+    {
+    }
+}
---
+++ Relay.Common/Option/RedisOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace Relay.Common
+{
+    /// <summary>
+    /// Redis配置（属性和appsettings.json的Redis节点保持一致）
+    /// </summary>
+    public sealed class RedisOptions : IConfigurableOptions
+    {
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        public bool Enable { get; set; }
+
+        /// <summary>
+        /// Redis连接
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// 键值前缀
+        /// </summary>
+        public string InstanceName { get; set; }
+    }
+}
