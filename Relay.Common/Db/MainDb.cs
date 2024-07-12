00bc4a1cd1a88e2805fb8a326a270dcb26097478
chusj <chusj.mail@qq.com>
2024-07-12 19:41:36 +0800

    2.Db相关配置类 和转换工具！

---
+++ Relay.Common/Db/MainDb.cs
@@ -0,0 +1,13 @@
+﻿namespace Relay.Common.Db
+{
+    /// <summary>
+    /// 主库
+    /// </summary>
+    public static class MainDb
+    {
+        /// <summary>
+        /// 当前数据连接实例
+        /// </summary>
+        public static string CurrentDbConnId = "Main";
+    }
+}
