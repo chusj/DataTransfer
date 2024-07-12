4f560ef6ba9a87bfce6d0ee57c19a6485d9390f7
chusj <chusj.mail@qq.com>
2024-07-12 19:57:27 +0800

    精简部分，冗余的代码

--- Relay.Api/Program.cs
+++ Relay.Api/Program.cs
@@ -1,14 +1,11 @@
 
-using Autofac.Extensions.DependencyInjection;
 using Autofac;
-using Relay.Extension;
-using Relay.IService;
-using Relay.Repository;
-using Relay.Service;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Relay.Common;
 using Relay.Common.Core;
+using Relay.Extension;
 
 namespace Relay.Api
 {
@@ -25,25 +22,19 @@ namespace Relay.Api
                     builder.RegisterModule<AutofacModuleRegister>();
                     builder.RegisterModule<AutofacPropertityModuleReg>();
                 })
-                 // 使用Options方式获取配置项，方式1
                  .ConfigureAppConfiguration((hostingContext, config) =>
                  {
                      hostingContext.Configuration.ConfigureApplication();
                  })
                  ;
             builder.ConfigureApplication();
+
             // Add services to the container. //asp.netcore 原生的依赖容器
 
-            //属性注入需要开启IControllerActivator ，默认是由 DefaultControllerActivator 负责实现，源码参考如下：
-            //https://github.com/dotnet/aspnetcore/blob/main/src/Mvc/Mvc.Core/src/Controllers/DefaultControllerActivator.cs
+            //属性注入需要开启IControllerActivator
+            builder.Services.Replace(ServiceDescriptor.Transient<IControllerActivator, ServiceBasedControllerActivator>());
 
-            
-            //这句代码第45行AddControllersAsServices() 作用相同
-            //builder.Services.Replace(ServiceDescriptor.Transient<IControllerActivator, ServiceBasedControllerActivator>());
-
-            builder.Services.AddControllers()
-                .AddControllersAsServices();  //原先的控制器只是一个类，此方法将控制器作为作为一个服务。
-            //AddControllersAsServices() 源码参考：https://github.com/dotnet/aspnetcore/blob/main/src/Mvc/Mvc.Core/src/DependencyInjection/MvcCoreMvcBuilderExtensions.cs 第132行
+            builder.Services.AddControllers();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -53,15 +44,8 @@ namespace Relay.Api
             builder.Services.AddAutoMapper(typeof(AutoMapperConfig));
             AutoMapperConfig.RegisterMappings();
 
-            //依赖注入(原生)
-            //builder.Services.AddScoped(typeof(IBaseRepository<>),typeof(BaseRepository<>));
-            //builder.Services.AddScoped(typeof(IBaseService<,>), typeof(BaseService<,>));
-
             //配置
             builder.Services.AddSingleton(new AppSettings(builder.Configuration));
-            
-            //使用Options方式获取配置项，方式2
-            //ConfigurableOptions.ConfigureApplication(builder.Configuration);
 
             builder.Services.AddAllOptionRegister();
 
