6045916bc53d8c136465926734aa5ec5f157f92e
chusj <chusj.mail@qq.com>
2024-07-12 16:21:25 +0800

    构造函数中的依赖，是从依赖注入容器中拿的； 但是构造函数的创建并不是从依赖注入容器中拿的，所以类的属性也不是从依赖注入容器拿的。

--- Relay.Api/Program.cs
+++ Relay.Api/Program.cs
@@ -34,10 +34,17 @@ namespace Relay.Api
             builder.ConfigureApplication();
             // Add services to the container. //asp.netcore 原生的依赖容器
 
-            //属性注入需要开启IControllerActivator
-            builder.Services.Replace(ServiceDescriptor.Transient<IControllerActivator, ServiceBasedControllerActivator>());
+            //属性注入需要开启IControllerActivator ，默认是由 DefaultControllerActivator 负责实现，源码参考如下：
+            //https://github.com/dotnet/aspnetcore/blob/main/src/Mvc/Mvc.Core/src/Controllers/DefaultControllerActivator.cs
 
-            builder.Services.AddControllers();
+            
+            //这句代码第45行AddControllersAsServices() 作用相同
+            //builder.Services.Replace(ServiceDescriptor.Transient<IControllerActivator, ServiceBasedControllerActivator>());
+
+            builder.Services.AddControllers()
+                .AddControllersAsServices();  //原先的控制器只是一个类，此方法将控制器作为作为一个服务。
+            //AddControllersAsServices() 源码参考：https://github.com/dotnet/aspnetcore/blob/main/src/Mvc/Mvc.Core/src/DependencyInjection/MvcCoreMvcBuilderExtensions.cs 第132行
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
