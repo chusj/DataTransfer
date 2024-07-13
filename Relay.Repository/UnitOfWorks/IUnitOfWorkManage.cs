b25ef99cac80cabaf3697444b1f08047a33aaa05
chusj <chusj.mail@qq.com>
2024-07-13 14:17:05 +0800

    工作单元业务类

---
+++ Relay.Repository/UnitOfWorks/IUnitOfWorkManage.cs
@@ -0,0 +1,16 @@
+﻿using SqlSugar;
+
+namespace Relay.Repository.UnitOfWorks
+{
+    /// <summary>
+    /// 工作单元接口(事务)
+    /// </summary>
+    public interface IUnitOfWorkManage
+    {
+        SqlSugarScope GetDbClient();
+        void BeginTran();
+        void CommitTran();
+        void RollbackTran();
+        UnitOfWork CreateUnitOfWork();
+    }
+}
