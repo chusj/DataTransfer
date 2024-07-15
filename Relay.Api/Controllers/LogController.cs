968227084fc791b952335e12f17b338130df80b8
chusj <chusj.mail@qq.com>
2024-07-15 16:50:04 +0800

    测试控制器

---
+++ Relay.Api/Controllers/LogController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Relay.IService;
+using Relay.Model;
+
+namespace Relay.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        private readonly IBaseService<AuditSqlLog, AuditSqlLogVo> _auditSqllogService;
+
+        public LogController(IBaseService<AuditSqlLog, AuditSqlLogVo> auditSqllogService)
+        {
+            _auditSqllogService = auditSqllogService;
+        }
+
+        //第18课测试
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            return await _auditSqllogService.Query();
+        }
+    }
+}
