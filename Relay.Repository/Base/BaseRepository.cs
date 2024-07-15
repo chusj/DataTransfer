f8cda636e49725eec344b51b8351d14aced13948
chusj <chusj.mail@qq.com>
2024-07-15 16:49:58 +0800

    仓储层修改，支持多库

--- Relay.Repository/Base/BaseRepository.cs
+++ Relay.Repository/Base/BaseRepository.cs
@@ -1,4 +1,6 @@
-﻿using SqlSugar;
+﻿using Relay.Repository.UnitOfWorks;
+using SqlSugar;
+using System.Reflection;
 
 namespace Relay.Repository
 {
@@ -7,24 +9,53 @@ namespace Relay.Repository
     /// </summary>
     public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class, new()
     {
-        private readonly ISqlSugarClient _dbBase;
-        public BaseRepository(ISqlSugarClient sqlSugarClient)
+        /// <summary>
+        /// 主库
+        /// </summary>
+        private readonly SqlSugarScope _dbBase;
+
+        private readonly IUnitOfWorkManage _unitOfWorkManage;
+
+        public ISqlSugarClient Db => _db;
+
+        private ISqlSugarClient _db
         {
-            _dbBase = sqlSugarClient;
+            get
+            {
+                ISqlSugarClient db = _dbBase;
+
+                //修改使用 model备注字段作为切换数据库条件，使用sqlsugar TenantAttribute存放数据库ConnId
+                //参考 https://www.donet5.com/Home/Doc?typeId=2246
+                var tenantAttr = typeof(TEntity).GetCustomAttribute<TenantAttribute>();
+                if (tenantAttr != null)
+                {
+                    //统一处理 configId 小写
+                    db = _dbBase.GetConnectionScope(tenantAttr.configId.ToString().ToLower());
+                    return db;
+                }
+
+                return db;
+            }
         }
 
-        public ISqlSugarClient Db => _dbBase;
 
-        public async Task<long> Add(TEntity entity)
+        public BaseRepository(IUnitOfWorkManage unitOfWorkManage)
         {
-            var insert = _dbBase.Insertable(entity);
-            return await insert.ExecuteReturnSnowflakeIdAsync();
+            _unitOfWorkManage = unitOfWorkManage;
+            _dbBase = unitOfWorkManage.GetDbClient();
         }
 
         public async Task<List<TEntity>> Query()
         {
-            //await Console.Out.WriteLineAsync($"仓储层 sqlsugar的Db实例HashCode ： {Db.GetHashCode()}");
-            return await _dbBase.Queryable<TEntity>().ToListAsync();
+            //await Console.Out.WriteLineAsync(Db.GetHashCode().ToString());
+            return await _db.Queryable<TEntity>().ToListAsync();
         }
+
+        public async Task<long> Add(TEntity entity)
+        {
+            var insert = _db.Insertable(entity);
+            return await insert.ExecuteReturnSnowflakeIdAsync();
+        }
+
     }
 }
--- Relay.Repository/Base/IBaseRepository.cs
+++ Relay.Repository/Base/IBaseRepository.cs
@@ -9,8 +9,17 @@ namespace Relay.Repository
     {
         ISqlSugarClient Db { get; }
 
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns></returns>
         Task<long> Add(TEntity entity);
 
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <returns>实体</returns>
         Task<List<TEntity>> Query();
     }
 }
