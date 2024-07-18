3453f84117144855cb9b389500140bd03fb6037a
chusj <chusj.mail@qq.com>
2024-07-18 14:49:07 +0800

    添加测试路由和方法，以及数据库数据变更

--- Relay.Api/Controllers/TenantController.cs
+++ Relay.Api/Controllers/TenantController.cs
@@ -6,6 +6,7 @@ using Relay.Model;
 using Relay.Model.Tenants;
 using Relay.Model.Vo;
 using Relay.Service;
+using SqlSugar;
 
 namespace Relay.Api
 {
@@ -17,17 +18,20 @@ namespace Relay.Api
         private readonly IBaseService<BusinessTable, BusinessTableVo> _bizServices;
         private readonly IBaseService<MultiBusinessTable, MultiBusinessTableVo> _multiBusinessService;
         private readonly IBaseService<SubLibraryBusinessTable, SubLibraryBusinessTableVo> _subLibBusinessService;
+        private readonly IBaseService<SysTenant, SysTenantVo> _sysTenantService;
         private readonly IUser _user;
 
         public TenantController(IUser user, 
             IBaseService<BusinessTable, BusinessTableVo> bizServices,
             IBaseService<MultiBusinessTable, MultiBusinessTableVo> multiBusinessService,
-            IBaseService<SubLibraryBusinessTable, SubLibraryBusinessTableVo> subLibBusinessService)
+            IBaseService<SubLibraryBusinessTable, SubLibraryBusinessTableVo> subLibBusinessService,
+            IBaseService<SysTenant, SysTenantVo> sysTenantService)
         {
             _user = user;
             _bizServices = bizServices;
             _multiBusinessService = multiBusinessService;
             _subLibBusinessService = subLibBusinessService;
+            _sysTenantService = sysTenantService;
         }
 
         /// <summary>
@@ -50,6 +54,25 @@ namespace Relay.Api
         public async Task<object> SubLibraryBusinessTable()
         {
             return await _subLibBusinessService.Query();
+        }
+
+        [HttpGet]
+        public async Task<object> GetTenantCache()
+        {
+            return await _sysTenantService.QueryWithCache();
+        }
+
+        [HttpGet]
+        public async Task<object> AddTenant()
+        {
+            return await _sysTenantService.Add(new SysTenant()
+            {
+                Id = SnowFlakeSingle.instance.getID(),
+                Name = "test name",
+                TenantType = TenantTypeEnum.Db,
+                ConfigId = "test config",
+                Status = false
+            });
         }
     }
 }
