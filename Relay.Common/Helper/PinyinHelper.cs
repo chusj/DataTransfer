f7ca0e11902bdfeacd4b2bf9062feaf38f8d611d
chusj <chusj.mail@qq.com>
2024-07-29 17:47:55 +0800

    引用包 NPinyin.Core，并添加工具类

---
+++ Relay.Common/Helper/PinyinHelper.cs
@@ -0,0 +1,39 @@
+﻿using NPinyin;
+using System.Text;
+
+namespace Relay.Common.Helper
+{
+    public class PinyinHelper : IHelper
+    {
+        /// <summary>
+        /// 获取给定中文姓名的拼音首字母缩写。
+        /// </summary>
+        /// <param name="name">中文姓名</param>
+        /// <returns>拼音首字母缩写</returns>
+        public static string GetInitials(string name)
+        {
+            var pinyin = Pinyin.GetPinyin(name);
+            var initials = new StringBuilder();
+
+            foreach (var part in pinyin.Split(' '))
+            {
+                if (!string.IsNullOrEmpty(part))
+                {
+                    initials.Append(part.Substring(0, 1).ToUpper());
+                }
+            }
+
+            return initials.ToString();
+        }
+
+        /// <summary>
+        /// 获取给定中文姓名的全拼。
+        /// </summary>
+        /// <param name="name">中文姓名</param>
+        /// <returns>全拼字符串</returns>
+        public static string GetFullPinyin(string name)
+        {
+            return Pinyin.GetPinyin(name);
+        }
+    }
+}
